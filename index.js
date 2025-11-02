dee058d4676f76308442200b1a92e931dfeb428b
crowrooster971 <wanlishajna@hotmail.com>
2025-11-02 12:09:14 +0200

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur l'application Retours Utilisateurs!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur le port ${PORT}`);
+});
